c2d5bc2c5b2238d19e194b4a6ac5f16cdd225eea
Geoff Turk <geoff@ic3.dev>
2023-04-24 01:47:54 +0200

    feat: fetch tag list from index

--- app/routes/groups.jsx
+++ app/routes/groups.jsx
@@ -10,27 +10,6 @@ export let meta = () => {
   return [{ title: 'Open Groups Hub' }]
 }
 
-let tagsData = [
-  { value: 'sustainable', count: 25 },
-  { value: 'recycle', count: 18 },
-  { value: 'regenerative', count: 38 },
-  { value: 'renewable', count: 30 },
-  { value: 'solar', count: 28 },
-  { value: 'Mutual Credit', count: 25 },
-  { value: 'Open Data', count: 33 },
-  { value: 'mutual aid', count: 20 },
-  { value: 'Open Source', count: 22 },
-  { value: 'wind power', count: 2 },
-  { value: 'biodynamic', count: 25 },
-  { value: 'ecodesign', count: 15 },
-  { value: 'sustainable development', count: 17 },
-  { value: 'resource recovery', count: 27 },
-  { value: 'Organic', count: 30 },
-  { value: 'rainforest', count: 15 },
-  { value: 'Fair Trade', count: 30 },
-  { value: 'Living systems', count: 11 }
-]
-
 export async function action({ request }) {
   let formData = await request.formData()
   let { _action, ...values } = Object.fromEntries(formData)
@@ -51,32 +30,53 @@ export async function loader({ request }) {
     for (let param of url.searchParams.entries()) {
       params[param[0]] = param[1]
     }
-    // Bail if there is no tag for searching
-    if (!!params.tags === false) return null
+    // If there is no tag for searching, load the tag list
+    if (!!params.tags === false) {
+      let response = await fetchGet(
+        `${process.env.PUBLIC_INDEX_URL}/nodes?schema=${process.env.PUBLIC_GROUPS_SCHEMA}&status=posted`
+      )
+      let nodesList = await response.json()
 
+      if (!response.ok) {
+        return new Response('Node list loading error', {
+          status: response.status
+        })
+      }
+
+      let tagsData = []
+      nodesList.data.map(node => {
+        node.tags.map(tag => {
+          let tagIndex = tagsData.findIndex(item => item.value === tag)
+          if (tagIndex > -1) {
+            tagsData[tagIndex].count++
+          } else {
+            tagsData.push({ value: tag, count: 1 })
+          }
+          return null
+        })
+        return null
+      })
+
+      return json({
+        tagsData: tagsData
+      })
+    }
+
+    // Otherwise fetch nodes from Index that match the tag
     let searchParams = getSearchParams(params)
-    // Fetch profiles from Index that match the tag
     let response = await fetchGet(
       `${process.env.PUBLIC_INDEX_URL}/nodes?${searchParams}&schema=${process.env.PUBLIC_GROUPS_SCHEMA}&status=posted`
     )
     let nodesList = await response.json()
 
     if (!response.ok) {
-      if (response.status === 404) {
-        return json({
-          params: params,
-          message: nodesList.errors?.[0].detail,
-          success: false
-        })
-      }
-
       return new Response('Node list loading error', {
         status: response.status
       })
     }
 
-    let nodes = []
     // Fetch profile data from each node
+    let nodes = []
     await Promise.all(
       nodesList.data.map(async node => {
         try {
@@ -92,6 +92,7 @@ export async function loader({ request }) {
 
           let payload = await promise.json()
 
+          // Add last_updated date/time from Index
           payload.last_updated = node.last_updated
           nodes.push(payload)
 
@@ -118,33 +119,37 @@ export async function loader({ request }) {
 export default function Index() {
   let loaderData = useLoaderData()
   let navigation = useNavigation()
+  let submit = useSubmit()
   let nodes = loaderData?.nodes
   let tag = loaderData?.params?.tags
-  let [viewTags, setViewTags] = useState(nodes ? false : true)
+  let tagsData = loaderData?.tagsData
+  let [showTags, setShowTags] = useState(nodes ? false : true)
   let [tagSelected, setTagSelected] = useState(tag ? tag : '')
-  let submit = useSubmit()
 
+  // handleTagClick() is called when a tag is clicked in the TagsCloud component
   function handleTagClick(tag) {
     let formData = new FormData()
     formData.append('_action', 'search')
     formData.append('tags', tag.value)
     submit(formData, { method: 'post' })
     setTagSelected(tag.value)
-    setViewTags(false)
+    setShowTags(false)
   }
 
+  // handleTagView() is called when the "Reset" button is clicked to clear the tag search
   function handleTagView() {
     let formData = new FormData()
     formData.append('_action', 'search')
     submit(formData, { method: 'post' })
-    setViewTags(!viewTags)
-    if (viewTags === false) {
+    setShowTags(!showTags)
+    if (showTags === false) {
       setTagSelected('')
     }
   }
 
   return (
     <div className="mt-2 flex flex-col items-center justify-center gap-2 md:mt-8 md:gap-8">
+      {/* TODO: Move this navigation to the root route */}
       <div className="flex justify-center gap-4 md:gap-8">
         <div className="w-20 rounded-lg bg-stone-500 p-2 text-center text-lg text-stone-50 shadow-xl dark:bg-stone-600 md:w-32 md:p-4 md:text-3xl">
           Groups
@@ -156,101 +161,106 @@ export default function Index() {
         </Link>
       </div>
       <div className="m-2 max-w-screen-xl justify-center rounded-md bg-stone-200 p-4 shadow-md dark:bg-stone-700 md:m-0">
-        <div className="flex items-center justify-center gap-4 md:gap-8">
-          {viewTags && (
-            <div className="text-center text-base italic text-stone-900 dark:text-stone-50 md:text-xl">
-              Select a tag
+        {/* Show the selected tag if there is one */}
+        {tagSelected && (
+          <div className="flex flex-col items-center">
+            <div className="pb-2 text-center text-lg font-bold text-teal-800 dark:text-teal-500 md:pb-4 md:text-2xl">
+              {tagSelected}
             </div>
-          )}
-          {!viewTags && (
             <button
               className="w-14 rounded-lg bg-stone-300 px-2 py-1 text-center text-xs hover:scale-105 active:scale-90 dark:bg-stone-400 dark:text-stone-950 md:w-24 md:px-4 md:py-2 md:text-base"
               onClick={() => handleTagView()}
             >
               Reset
             </button>
-          )}
-        </div>
-        {viewTags && (
-          <TagsCloud
-            minSize={14}
-            maxSize={48}
-            tags={tagsData}
-            handleTagClick={handleTagClick}
-          />
-        )}
-        {tagSelected && (
-          <div className="pt-2 text-center text-lg font-bold text-teal-800 dark:text-teal-500 md:pt-4 md:text-2xl">
-            {tagSelected}
           </div>
         )}
+        <div className="flex items-center justify-center gap-4 md:gap-8">
+          {/* Show the tag cloud if there is no tag selected */}
+          {!tagSelected && (
+            <div>
+              <div className="text-center text-base italic text-stone-900 dark:text-stone-50 md:text-xl">
+                Select a tag
+              </div>
+              <TagsCloud
+                minSize={14}
+                maxSize={48}
+                tags={tagsData || []}
+                handleTagClick={handleTagClick}
+              />
+            </div>
+          )}
+        </div>
       </div>
       <div className="container mx-auto max-w-3xl px-4">
-        {!viewTags && nodes?.length > 0
-          ? nodes.map((node, index) => {
-              return (
-                <div key={index} className="py-2 md:py-4">
-                  <div className="flex flex-col justify-between bg-white p-4 shadow-lg dark:bg-black">
-                    <div className="flex">
-                      <img
-                        className="h-24 flex-none md:w-36 lg:h-auto"
-                        src={node?.image}
-                        alt={node?.name}
-                      />
-                      <div className="flex flex-col px-4 md:px-8">
-                        <div className="text-xl font-bold text-stone-900 dark:text-stone-200 md:text-3xl">
-                          {node?.name}
-                        </div>
-                        <a
-                          className="mb-2 text-sm text-blue-700 dark:text-blue-500 md:text-lg"
-                          href={`https://${node?.primary_url}`}
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          {node?.primary_url}
-                        </a>
-                        <div className="text-xs font-bold text-stone-400 dark:text-stone-500 md:text-base">
-                          {node?.geographic_scope?.toLocaleUpperCase()}
-                        </div>
-                        <div className="font-italic text-xs text-stone-600 dark:text-stone-400 md:text-base">
-                          {node?.locality}
-                          {node?.locality && node?.country_name
-                            ? ','
-                            : null}{' '}
-                          {node?.country_name}
-                        </div>
+        {/* Show the list of nodes if there are any */}
+        {nodes?.length > 0 ? (
+          nodes.map((node, index) => {
+            return (
+              <div key={index} className="py-2 md:py-4">
+                <div className="flex flex-col justify-between bg-white p-4 shadow-lg dark:bg-black">
+                  <div className="flex">
+                    <img
+                      className="h-24 flex-none md:w-36 lg:h-auto"
+                      src={node?.image}
+                      alt={node?.name}
+                    />
+                    <div className="flex flex-col px-4 md:px-8">
+                      <div className="text-xl font-bold text-stone-900 dark:text-stone-200 md:text-3xl">
+                        {node?.name}
+                      </div>
+                      <a
+                        className="mb-2 text-sm text-blue-700 dark:text-blue-500 md:text-lg"
+                        href={`https://${node?.primary_url}`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {node?.primary_url}
+                      </a>
+                      <div className="text-xs font-bold text-stone-400 dark:text-stone-500 md:text-base">
+                        {node?.geographic_scope?.toLocaleUpperCase()}
+                      </div>
+                      <div className="font-italic text-xs text-stone-600 dark:text-stone-400 md:text-base">
+                        {node?.locality}
+                        {node?.locality && node?.country_name ? ',' : null}{' '}
+                        {node?.country_name}
                       </div>
                     </div>
-                    <div className="my-2 text-sm text-stone-900 dark:text-stone-200 md:my-4 md:text-base md:text-lg">
-                      {node?.description}
-                    </div>
-                    <div className="flex flex-wrap items-center">
-                      {node?.tags.map((tag, index) => {
-                        return (
-                          <div
-                            key={index}
-                            className="m-1 rounded-3xl bg-teal-800 px-2 py-1 text-xs text-stone-50 dark:bg-teal-700 md:m-2 md:px-4 md:py-2 md:text-base"
-                          >
-                            {tag}
-                          </div>
-                        )
-                      })}
-                    </div>
-                    <div className="mt-2 text-right text-xs text-stone-400 dark:text-stone-500 md:mt-4 md:text-base">
-                      Last Updated:{' '}
-                      {new Date(node?.last_updated * 1000)
-                        .toISOString()
-                        .replace(/T.+/, '')}
-                    </div>
+                  </div>
+                  <div className="my-2 text-sm text-stone-900 dark:text-stone-200 md:my-4 md:text-base md:text-lg">
+                    {node?.description}
+                  </div>
+                  <div className="flex flex-wrap items-center">
+                    {node?.tags.map((tag, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className="m-1 rounded-3xl bg-teal-800 px-2 py-1 text-xs text-stone-50 dark:bg-teal-700 md:m-2 md:px-4 md:py-2 md:text-base"
+                        >
+                          {tag}
+                        </div>
+                      )
+                    })}
+                  </div>
+                  <div className="mt-2 text-right text-xs text-stone-400 dark:text-stone-500 md:mt-4 md:text-base">
+                    Last Updated:{' '}
+                    {new Date(node?.last_updated * 1000)
+                      .toISOString()
+                      .replace(/T.+/, '')}
                   </div>
                 </div>
-              )
-            })
-          : !viewTags && (
-              <div className="flex flex-col items-center justify-center gap-2 md:gap-4">
-                {navigation.state !== 'idle' ? 'Loading...' : 'No results'}
               </div>
-            )}
+            )
+          })
+        ) : (
+          <div className="flex flex-col items-center justify-center gap-2 md:gap-4">
+            {navigation.state !== 'idle'
+              ? 'Loading...'
+              : tagSelected
+              ? 'No results'
+              : ''}
+          </div>
+        )}
       </div>
     </div>
   )
